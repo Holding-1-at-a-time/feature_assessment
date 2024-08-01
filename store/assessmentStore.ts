5f070e71dad07383a661fbed6fe685fa21de9f98
holding-1-at-a-time <rromerojr1@gmail.com>
2024-07-31 21:17:12 -0500

    refactored

--- store/assessmentStore.ts
+++ store/assessmentStore.ts
@@ -1,29 +1,28 @@
 import create from 'zustand';
 
 const useAssessmentStore = create((set) => ({
-  activeHotspot: null,
-  assessmentId: null,
-  setActiveHotspot: (hotspot) => set({ activeHotspot: hotspot }),
-  setAssessmentId: (id) => set({ assessmentId: id }),
+    activeHotspot: null,
+    assessmentId: null,
+    setActiveHotspot: (hotspot) => set({ activeHotspot: hotspot }),
+    setAssessmentId: (id) => set({ assessmentId: id }),
 }));
 
 export default useAssessmentStore;
 import useAssessmentStore from '@/store/assessmentStore';
 
 const VehicleAssessmentContainer = () => {
-  const { activeHotspot, setActiveHotspot, assessmentId } = useAssessmentStore();
+    const { activeHotspot, setActiveHotspot, assessmentId } = useAssessmentStore();
 
-  const handleHotspotClick = (part) => {
-    setActiveHotspot(part);
-  };
-
-  return (
+    const handleHotspotClick = (part) => {
+        setActiveHotspot(part);
+    }
+}
+return (
     <div>
-      <VehicleHotspotAssessment onHotspotClick={handleHotspotClick} />
-      {activeHotspot && <ConditionDetailForm activeHotspot={activeHotspot} />}
-      <FileUploadsComponent />
-      <AdaptiveQuestionnaire vehicleType="sedan" reportedIssues={[]} />
-      {assessmentId && <RealTimeSummary assessmentId={assessmentId} />}
-    </div>
-  );
-};
+    <VehicleHotspotAssessment onHotspotClick= { handleHotspotClick } />
+{ activeHotspot && <ConditionDetailForm activeHotspot={ activeHotspot } /> }        < FileUploadsComponent />
+        <AdaptiveQuestionnaire vehicleType = "sedan" reportedIssues = { []} />
+            { assessmentId && <RealTimeSummary assessmentId={ assessmentId } />}
+</div>
+    );
+}
